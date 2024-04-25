cd56768cbc4182682bc6befaba0072544440d34d
yagyagoel1 <yagyagoel87@gmail.com>
2024-04-26 02:38:18 +0530

    added a route to get all the messages of a user

---
+++ src/app/api/get-messages/route.ts
@@ -0,0 +1,74 @@
+import { getServerSession } from "next-auth";
+import { User } from "next-auth";
+import { authOptions } from "../sign-up/auth/[...nextuath]/options";
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User.model";
+import mongoose from "mongoose";
+
+export async function GET(request: Request) {
+  await dbConnect();
+  const session = await getServerSession(authOptions);
+  const user = session?.user as User;
+  if (!session || !session.user) {
+    return Response.json(
+      {
+        success: false,
+        message: "not authenticated",
+      },
+      {
+        status: 401,
+      }
+    );
+  }
+
+  const userId = new mongoose.Types.ObjectId(user._id);
+  try {
+    const user = await UserModel.aggregate([
+      {
+        $match: {
+          id: userId,
+        },
+      },
+      {
+        $unwind: "$messages",
+      },
+      {
+        $sort: { "messages.createdAt": -1 },
+      },
+      {
+        $group: { _id: "$_id", messages: { $push: "$messages" } },
+      },
+    ]);
+    if (!user || user.length === 0) {
+      return Response.json(
+        {
+          success: false,
+          message: "user doesnt exist",
+        },
+        {
+          status: 404,
+        }
+      );
+    }
+    return Response.json(
+      {
+        success: false,
+        message: user[0].messages,
+      },
+      {
+        status: 200,
+      }
+    );
+  } catch (error) {
+    console.error("an unexpected error occured", error);
+    return Response.json(
+      {
+        success: false,
+        message: "an unexpected error occured",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
