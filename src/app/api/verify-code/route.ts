5f51e5acde39df39ebd7be0a39f3acdff41f12a3
yagyagoel1 <yagyagoel87@gmail.com>
2024-04-26 01:53:00 +0530

    added a route to check the verification code

---
+++ src/app/api/verify-code/route.ts
@@ -0,0 +1,82 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User.model";
+import { usernameValidation } from "@/schemas/signUpSchema";
+import { verifySchema } from "@/schemas/verifySchema";
+import { date, z } from "zod";
+
+export async function POST(request: Request) {
+  await dbConnect();
+  try {
+    const { username, code } = await request.json();
+    const decodedUsername = decodeURIComponent(username);
+    const usernameCheck = usernameValidation.safeParse(username);
+    const codeCheck = verifySchema.safeParse(code);
+    if (!codeCheck.success || !username.success) {
+      return Response.json(
+        {
+          success: false,
+          message: "enter a valid username and code",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+    const user = await UserModel.findOne({ username: decodedUsername });
+    if (!user) {
+      return Response.json(
+        {
+          success: false,
+          message: "user does not exist",
+        },
+        {
+          status: 400,
+        }
+      );
+    }
+    const isCodeValid = user.verifyCode === code;
+    const isCodeNotExpired = new Date(user.verifyCodeExpiry) > new Date();
+    if (isCodeValid && isCodeNotExpired) {
+      user.isVerified = true;
+      await user.save();
+      return Response.json(
+        {
+          success: true,
+          message: "account verified successfully",
+        },
+        {
+          status: 200,
+        }
+      );
+    } else if (!isCodeNotExpired) {
+      return Response.json(
+        {
+          success: false,
+          message: "code is expired ,please signup again to get a new code",
+        },
+        {
+          status: 400,
+        }
+      );
+    } else
+      return Response.json(
+        {
+          success: false,
+          message: "code is incorrect",
+        },
+        {
+          status: 400,
+        }
+      );
+  } catch (error) {
+    return Response.json(
+      {
+        success: false,
+        message: "error verifying user",
+      },
+      {
+        status: 500,
+      }
+    );
+  }
+}
