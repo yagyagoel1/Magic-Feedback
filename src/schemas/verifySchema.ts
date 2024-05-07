1c96e260651bcfc9f6dd5dfe51514c77652a345b
yagyagoel1 <yagyagoel87@gmail.com>
2024-05-07 07:43:02 +0530

    fixed schema

--- src/schemas/verifySchema.ts
+++ src/schemas/verifySchema.ts
@@ -1,5 +1,7 @@
 import { z } from "zod";
+import { usernameValidation } from "./signUpSchema";
 
 export const verifySchema = z.object({
+  username: usernameValidation,
   code: z.string().length(6, "verification code must be 6 digits"),
 });
