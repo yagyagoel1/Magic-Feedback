7dfa32a591867a70a79239398481b16841ed5f50
yagyagoel1 <yagyagoel87@gmail.com>
2024-05-07 06:31:11 +0530

    removed some unneccessary imports

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
-export { default } from "next-auth/middleware";
 export async function middleware(request: NextRequest) {
   const token = await getToken({ req: request });
   const url = request.nextUrl;
